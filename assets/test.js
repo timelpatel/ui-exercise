40f529ceb2bd3f9150b41d29c6433704b6c57644
Timel Patel <timel.patel@timelab.co.uk>
2017-11-24 10:49:20 +0000

    Exercise 3

--- assets/test.js
+++ assets/test.js
@@ -1,1 +1,12 @@
 /* Valtech UI Test JS */
+
+$(function () {
+
+    $( "#toggle-nav" ).click(function() {
+
+        $( "#sidebar" ).toggleClass( "closed" );
+        $( "#content" ).toggleClass( "full" );
+
+    })
+
+});
